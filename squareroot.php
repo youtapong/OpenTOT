6f0f8056027342a6c8cbc734a6804c8037cbd4d6
WarisDev <warissara13@gmail.com>
2020-03-11 14:28:09 +0700

    edit squareroot.php

--- squareroot.php
+++ squareroot.php
@@ -1,8 +1,8 @@
-
 <input id="inpN" type="text" oninput="SRoot(this.value)" onchange="SRoot(this.value)">
 <p>resault: <span id="SRoot"></span></p>
-
+<script>
 function SRoot(num){
-  document.getElementById("SRoot").innerHTML = sqrt(num);
-  
+    document.getElementById("SRoot").innerHTML = Math.sqrt(num); 
 }
+</script>
+
